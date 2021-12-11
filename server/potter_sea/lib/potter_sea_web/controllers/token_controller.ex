df5c38c540804fcc4d57555b2d89dd12348bfd45
Dogukan Zorlu <dogukanzrlu@gmail.com>
2021-12-11 11:08:22 +0300

    initial mongo test

--- server/potter_sea/lib/potter_sea_web/controllers/token_controller.ex
+++ server/potter_sea/lib/potter_sea_web/controllers/token_controller.ex
@@ -1,43 +1,25 @@
 defmodule PotterSeaWeb.TokenController do
   use PotterSeaWeb, :controller
 
-  alias PotterSea.NFT
-  alias PotterSea.NFT.Token
-
-  action_fallback PotterSeaWeb.FallbackController
-
   def index(conn, _params) do
-    tokens = NFT.list_tokens()
+    tokens = Mongo.find(:mongo, "tokens", %{})
     render(conn, "index.json", tokens: tokens)
   end
 
   def create(conn, %{"token" => token_params}) do
-    with {:ok, %Token{} = token} <- NFT.create_token(token_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.token_path(conn, :show, token))
-      |> render("show.json", token: token)
-    end
+    token = %{
+      token_id: "uuid",
+      price: token_params["price"],
+      ipfs_hash: token_params["ipfs_hash"]
+
+    }
+
+    Mongo.insert_one(:mongo, "tokens", token)
+    render(conn, "token.json", token: token)
   end
 
   def show(conn, %{"id" => id}) do
-    token = NFT.get_token!(id)
+    token = Mongo.find(:mongo, "tokens", %{token_id: id})
     render(conn, "show.json", token: token)
-  end
-
-  def update(conn, %{"id" => id, "token" => token_params}) do
-    token = NFT.get_token!(id)
-
-    with {:ok, %Token{} = token} <- NFT.update_token(token, token_params) do
-      render(conn, "show.json", token: token)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    token = NFT.get_token!(id)
-
-    with {:ok, %Token{}} <- NFT.delete_token(token) do
-      send_resp(conn, :no_content, "")
-    end
   end
 end
