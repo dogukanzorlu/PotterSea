74ec72c1b6d619ff193e2062a5b4278f5024281e
Dogukan Zorlu <dogukanzrlu@gmail.com>
2021-12-11 11:08:08 +0300

    token controller view fixed

--- server/potter_sea/lib/potter_sea_web/views/token_view.ex
+++ server/potter_sea/lib/potter_sea_web/views/token_view.ex
@@ -2,12 +2,19 @@ defmodule PotterSeaWeb.TokenView do
   use PotterSeaWeb, :view
   alias PotterSeaWeb.TokenView
 
+  defimpl Jason.Encoder, for: BSON.ObjectId do
+    def encode(val, _opts \\ []) do
+      BSON.ObjectId.encode!(val)
+      |> Jason.encode!()
+    end
+  end
+
   def render("index.json", %{tokens: tokens}) do
     %{data: render_many(tokens, TokenView, "token.json")}
   end
 
   def render("show.json", %{token: token}) do
-    %{data: render_one(token, TokenView, "token.json")}
+    %{data: render_one(token.docs, TokenView, "token.json")}
   end
 
   def render("token.json", %{token: token}) do
